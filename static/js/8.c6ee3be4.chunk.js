4acb06294a00731754b3ad9a10a5d0b3f24483ae
Shakil Ahmad <ahmad.shakil444@gmail.com>
2022-09-12 23:54:15 +0530

    Updates

---
+++ static/js/8.c6ee3be4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio-app"]=this["webpackJsonpportfolio-app"]||[]).push([[8],{87:function(t,e,p){"use strict";p.r(e);p(0),p(89);var a=p(1);e.default=function(t){var e=t.title;return Object(a.jsx)("div",{className:"header",children:e})}}}]);
+//# sourceMappingURL=8.c6ee3be4.chunk.js.map
